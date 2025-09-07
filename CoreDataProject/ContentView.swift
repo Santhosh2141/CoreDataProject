3c7543b796d1cff4edad4c17956c6a2fa9f39469
Santhosh2141 <snthshsrinivas@gmail.com>
2025-09-07 20:43:31 +0530

    Filering using NSPredicate

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct Student: Hashable{
     var name: String
@@ -18,14 +19,24 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
     
+    // predicate is used for filtering. %@ means we can use any value given after the comma.
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == '%@'","Star Wars")) var ships: FetchedResults<Ship>
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe < '%@'","F")) var ships: FetchedResults<Ship>
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe IN '%@'",["Aliens", "Firefly", "StarTrek"])) var ships: FetchedResults<Ship>
+//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH '%@'","E")) var ships: FetchedResults<Ship>
+    // this makes it case insensitive
+    //    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH[c] '%@'","E")) var ships: FetchedResults<Ship>
+    //    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH '%@'","E")) var ships: FetchedResults<Ship>
     var body: some View {
         VStack {
-            List{
-                // .self means we use the whole object as the keyPath
-                ForEach([2,4,6,8,10], id: \.self){
-                    Text("\($0)")
-                }
+            List(ships, id: \.self){ ship in
+                Text(ship.name ?? "Unkwon Name")
             }
+                // .self means we use the whole object as the keyPath
+//                ForEach([2,4,6,8,10], id: \.self){
+//                    Text("\($0)")
+//                }
+            
             // id self will work ww Hashable data only. string ints are hashable by default
             List(students, id: \.self){ student in
                 Text(student.name)
@@ -38,6 +49,19 @@ struct ContentView: View {
                 Button("Add Wizard"){
                     let wizard = Wizard(context: moc)
                     wizard.name = "Harry Potter"
+                    
+                }
+                Spacer()
+                Button("Add Ships"){
+                    let ship1 = Ship(context: moc)
+                    ship1.name = "EnterPrise"
+                    ship1.universe = "Star Trek"
+                    let ship2 = Ship(context: moc)
+                    ship2.name = "Defiant"
+                    ship2.universe = "Star Trek"
+                    let ship3 = Ship(context: moc)
+                    ship3.name = "Milleniam Falcon"
+                    ship3.universe = "Star Wars"
                     
                 }
                 Spacer()
