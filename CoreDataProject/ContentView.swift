94151ce01f9d3dea70793cb47bf1d841921bb381
Santhosh2141 <snthshsrinivas@gmail.com>
2025-09-07 13:44:40 +0530

    working w \.self

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -7,13 +7,25 @@
 
 import SwiftUI
 
+struct Student: Hashable{
+    var name: String
+}
 struct ContentView: View {
+    
+    // Saving hashvalues will be a problem. cuz when we close and open again hashvalue might be changed and will cause problems
+    @State private var students = [Student(name: "Harry Potter"), Student(name: "Hermoine Granger"), Student(name: "Ron Weasley")]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List{
+                // .self means we use the whole object as the keyPath
+                ForEach([2,4,6,8,10], id: \.self){
+                    Text("\($0)")
+                }
+            }
+            // id self will work ww Hashable data only. string ints are hashable by default
+            List(students, id: \.self){ student in
+                Text(student.name)
+            }
         }
         .padding()
     }
