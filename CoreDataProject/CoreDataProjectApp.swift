814023d7478b7f2726054277a837db39ea1f8e81
Santhosh2141 <snthshsrinivas@gmail.com>
2025-09-07 13:08:06 +0530

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Santhosh Srinivas on 07/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
