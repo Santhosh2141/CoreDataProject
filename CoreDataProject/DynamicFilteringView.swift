b099104665a724fa5c4cbc672f52fe7f2ae8f769
Santhosh2141 <snthshsrinivas@gmail.com>
2025-09-08 21:30:39 +0530

    accept a string parameter controlling which predicate is applied using an enum and accept a SortDescriptor passed to FilteredList

--- CoreDataProject/DynamicFilteringView.swift
+++ CoreDataProject/DynamicFilteringView.swift
@@ -8,18 +8,35 @@
 import CoreData
 import SwiftUI
 
+enum Predicate: String, CaseIterable, CustomStringConvertible {
+    case beginsWith = "BEGINSWITH"
+    case contains = "CONTAINS"
+    case containsCI = "CONTAINS[c]"
+    
+    var description: String { rawValue }
+}
+
 struct DynamicFilteringView: View {
     
     @Environment(\.managedObjectContext) var moc
     @State private var lastNameFilter = "A"
     // we cant use @FetchRequests as we have to update it based on another value. Based on the changed state value. this will affect the fetch req value outside. so we have to initialise it inside the view. so we create a new view
+//    private var stringParameter = "BEGINSWITH"
+    @State private var chooseParameter = Predicate.beginsWith
     var body: some View {
         VStack{
-            FilteredList(filterKey: "lastName", filterValue: lastNameFilter){ (singer: Singer) in
+            Form{
+                // .allcases makes it an array of all cases so that we can iterate
+                Picker("Choose an Option to Filter", selection: $chooseParameter) {
+                    ForEach(Predicate.allCases, id: \.self) { type in
+                        Text(type.description)
+                    }
+                }
+            }
+            FilteredList(stringParameter: chooseParameter.rawValue,sort: [SortDescriptor<Singer>(\.firstName)],filterKey: "lastName", filterValue: lastNameFilter){ (singer: Singer) in
                 // this is an NSManagedObject and not a singer object. so we tell that its a singer class
                 Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
             }
-            
             Button("Add Example"){
                 let taylor =  Singer(context: moc)
                 taylor.firstName = "Taylor"
--- CoreDataProject/FilteredList.swift
+++ CoreDataProject/FilteredList.swift
@@ -12,6 +12,7 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
     @FetchRequest(sortDescriptors: []) var fetchRequest: FetchedResults<T>
     // gimme a content closure that accepts a T and returns a contentView
     let content: (T) -> Content
+//    let stringParameter = "BEGINSWITH"
     var body: some View {
         List(fetchRequest, id: \.self){ item in
 //            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
@@ -19,10 +20,10 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
         }
     }
     
-    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+    init(stringParameter: String, sort: [SortDescriptor<T>], filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
         // this _fetchRequest means, we are not writing it to the fetchedResults. we are writing it to a completely new fetchRequest object
         // using %@ adds a single qoute. but the var name shouldnt be in quote. so we use %k
-        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        _fetchRequest = FetchRequest<T>(sortDescriptors: sort, predicate: NSPredicate(format: "%K \(stringParameter) %@", filterKey, filterValue))
         // this is a nonescaping parameter for later on use. so we use it as escaping so that it can be used later on
         self.content = content
     }
